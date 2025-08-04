5be0f0523918b75f68c15e0d6445dad58d980b63
ntqueprince <138240895+ntqueprince@users.noreply.github.com>
2025-08-04 10:01:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12077,7 +12077,7 @@ uploadBtn.style.display = "none";    // Upload button ko hide kar do
   { "Sr.": "36", "Insurer Name": "PB Partner Agent Team No.", "RSA and Toll Free Number": "1800-120-800", "Claim No.": "" },
   { "Sr.": "37", "Insurer Name": "PB_Mail", "RSA and Toll Free Number": "SUPPORT@PBPARTNER.COM", "Claim No.": "" },
   { "Sr.": "38", "Insurer Name": "Paisa Bazaar.com Team No.", "RSA and Toll Free Number": "1800-208-8877", "Claim No.": "" },
-  { "Sr.": "39", "Insurer Name": "PB_2W Renewal Team No.", "RSA and Toll Free Number": "0124-6138301", "Claim No.": "" }]
+  { "Sr.": "39", "Insurer Name": "PB_2W Renewal Team No.", "RSA and Toll Free Number": "0124-6138301", "Claim No.": "" }
 
     ];
 
