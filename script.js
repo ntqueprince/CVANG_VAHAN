afb7bbca562956802a130cc3219466b72dc20541
ntqueprince <138240895+ntqueprince@users.noreply.github.com>
2026-05-20 18:11:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const db = getDatabase(app);
 const imagesRef = dbRef(db, 'images');
 
 // Cloudinary configuration (from structure.html)
-const cloudName = 'dokuxlyfy'; // Replace with your Cloudinary cloud name
+const cloudName = 'dugehr9nl'; // Replace with your Cloudinary cloud name
 const uploadPreset = 'anonymous_upload'; // Replace with your Cloudinary upload preset
 // ✅ Handle paste event for images
 document.addEventListener("paste", function (event) {
